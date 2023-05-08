b258163319bdcc06740c42bb40c903a44a9bd716
Gloria Dal Santo <gloria.dalsanto@outlook.it>
2023-05-08 09:38:32 +0300

    Change config for sparse training

--- config.py
+++ config.py
@@ -2,22 +2,23 @@ samplerate = 48000
 dir_path = "./output"
 
 # dataset
-num = 256                   # dataaset size
-min_nfft = 8*samplerate     # min number of fft points M_min
-max_nfft = 10*samplerate    # max number of fft points M_max
-split = 0.78125             # trianing / validation split
+num = 10*samplerate                   # dataaset size
+min_nfft = samplerate     # min number of fft points M_min
+max_nfft = samplerate    # max number of fft points M_max
+split = 0.8 # 0.78125             # trianing / validation split
 shuffle = True              # if on shuffle the data after every epoch
 
 
 # training
-batch_size = 4
-max_epochs = 15
+batch_size = 2000
+max_epochs = 50
 
 
 # optimizer
 lr = 1e-3                   # leaning rate
-alpha = 300                 # temporal loss scaling factor
-
+alpha = 600                 # temporal loss scaling factor
+is_global = True            # if true computes the loss on the sum of the channels (overall )
+beta = 5                    # global loss scaling factor
 # netowrk 
 gain_per_sample = 0.9999    # gamma
 delays = [809., 877., 937., 1049., 1151., 1249., 1373., 1499.]
